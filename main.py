431d6cafd93ea66acc889ab1d7d7c3bb9529a0c2
Codynator <113552830+Codynator@users.noreply.github.com>
2024-05-28 22:25:55 +0200

    Unused code cleanup

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ from subprocess import run, CalledProcessError, CompletedProcess
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def check_range(pattern: list[int], position: int = -1, start_val: int = 0, end_val: int = 1) -> None:
-    if start_val >= end_val:
-        end_val = start_val + 1
-
-    for _i in range(start_val, end_val):
-        pattern[position] = _i
-        print(linux_ping('.'.join(map(str, pattern))))
-
-
 def return_range(pattern: list[int], position: int = -1, start_val: int = 0, end_val: int = 1) -> set:
     if start_val >= end_val:
         end_val = start_val + 1
@@ -35,7 +26,6 @@ def linux_ping(_host: str) -> tuple[str, bool]:
 
 if __name__ == '__main__':
     ipPattern: list[int] = [192, 168, 1, 1]
-    # check_range(ipPattern, position=-1, start_val=0, end_val=10)
     ipAddresses: set = return_range(ipPattern, end_val=10)
     pingsResult: set = set()
 
