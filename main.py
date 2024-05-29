78d38e0c4411fef9789ec6cbea9b016eb203ab72
Codynator <113552830+Codynator@users.noreply.github.com>
2024-05-29 23:23:53 +0200

    Added blacklisting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from subprocess import run, CalledProcessError, CompletedProcess
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def return_range(pattern: list[int], position: int = -1, start_val: int = 0, end_val: int = 1) -> set:
+def return_range(pattern: list[int], position: int = -1, start_val: int = 0, end_val: int = 1) -> set[str]:
     if start_val >= end_val:
         end_val = start_val + 1
 
@@ -26,8 +26,17 @@ def linux_ping(_host: str) -> tuple[str, bool]:
 
 if __name__ == '__main__':
     ipPattern: list[int] = [192, 168, 1, 1]
-    ipAddresses: set = return_range(ipPattern, end_val=10)
-    pingsResult: set = set()
+    ipAddresses: set[str] = return_range(ipPattern, end_val=10)
+    pingsResult: set[tuple] = set()
+    blacklist: set[str] = set()
+
+    addToBlacklist: bool = input("Do you want to add IPs to a blacklist? (y/n) ").lower().startswith('y')
+    if addToBlacklist:
+        numOfIPs: int = int(input('Input a number of addresses to blacklist: '))
+        for i in range(numOfIPs):
+            blacklist.add(input(f'{i + 1}. '))
+
+    ipAddresses = ipAddresses - blacklist
 
     with ThreadPoolExecutor() as executor:
         futures: set = {executor.submit(linux_ping, host) for host in ipAddresses}
