24d3b6aa3d2b8b8daa85661b7bb66e0463f9e828
Codynator <113552830+Codynator@users.noreply.github.com>
2024-06-16 22:15:04 +0200

    Preparation for integration with the main.py file

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from webbrowser import open as wb_open
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
 
+ipPattern: list[int] = [192, 168, 1, 0]
+
 
 class HeaderFrame(ctk.CTkFrame):
     def __init__(self, master):
@@ -15,11 +17,8 @@ class HeaderFrame(ctk.CTkFrame):
 
         self.title = ctk.CTkLabel(self, text="Network Scanner", font=("", 20))
         self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
-        self.subtitle = ctk.CTkLabel(self, text="Version 0.11")
+        self.subtitle = ctk.CTkLabel(self, text="Version 0.12")
         self.subtitle.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="ew")
-
-        self.spacer = ctk.CTkLabel(self, text="")
-        self.spacer.grid(row=2, column=0, padx=0, pady=0, sticky="nsew")
 
         self.themeLabel = ctk.CTkLabel(self, text="Current theme:")
         self.themeLabel.grid(row=3, column=0, padx=10, pady=0, sticky="ew")
@@ -40,35 +39,54 @@ class MainFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        self.columnconfigure((0, 1, 2), weight=1)
+
         self.osLabel = ctk.CTkLabel(self, text="Choose your OS:")
         self.osLabel.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
         self.osMenu = ctk.CTkOptionMenu(self, values=['Linux', 'Windows'], command=self.set_preview_command)
-        self.osMenu.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="w")
+        self.osMenu.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="we")
 
         self.previewLabel = ctk.CTkLabel(self, text="Command preview:")
         self.previewLabel.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="w")
         self.previewEntry = ctk.CTkEntry(self)
-        self.previewEntry.grid(row=1, column=1, padx=10, pady=(0, 10), sticky="w")
+        self.previewEntry.grid(row=1, column=1, padx=10, pady=(0, 10), sticky="we")
         self.previewEntry.insert(0, "ping -c 1 -w 3")
 
         self.patternLabel = ctk.CTkLabel(self, text="IP pattern:")
         self.patternLabel.grid(row=0, column=2, padx=10, pady=(10, 0), sticky="w")
-        self.patternEntry = ctk.CTkEntry(self, placeholder_text="192.168.1.0")
-        self.patternEntry.grid(row=1, column=2, padx=10, pady=(0, 10), sticky="w")
+        self.patternEntry = ctk.CTkEntry(self, placeholder_text="default: 192.168.1.0")
+        self.patternEntry.grid(row=1, column=2, padx=10, pady=(0, 10), sticky="we")
+        self.patternEntry.insert(0, '.'.join(map(str, ipPattern)))
 
-        # self.searchButton = ctk.CTkButton(self, text="Scan")
+        self.addressesRangeLabel = ctk.CTkLabel(self, text="Designate a range of IP addresses to scan",
+                                                fg_color=("grey80", "grey20"), corner_radius=10)
+        self.addressesRangeLabel.grid(row=2, column=0, columnspan=2, padx=10, pady=(10, 0), sticky="we")
+        self.rangeFromEntry = ctk.CTkEntry(self, placeholder_text="from")
+        self.rangeFromEntry.grid(row=3, column=0, padx=10, pady=10, sticky="we")
+        self.rangeToEntry = ctk.CTkEntry(self, placeholder_text="to")
+        self.rangeToEntry.grid(row=3, column=1, padx=10, pady=10, sticky="we")
+
+        self.resultLabel = ctk.CTkLabel(self, text="")
+        self.resultLabel.grid(row=3, column=2, padx=10, pady=10, sticky="we")
+
+        self.scanButton = ctk.CTkButton(self, text="Scan")
+        self.scanButton.grid(row=4, column=1, pady=10, sticky="we")
 
     @staticmethod
-    def clear_entry(_entry: ctk.CTkEntry):
+    def clear_entry(_entry: ctk.CTkEntry) -> None:
         _entry.delete(0, len(_entry.get()))
 
-    def set_preview_command(self, os_name: str):
+    def set_preview_command(self, os_name: str) -> None:
         self.clear_entry(self.previewEntry)
 
         if os_name == "Linux":
             self.previewEntry.insert(0, "ping -c 1 -w 3")
         else:
             self.previewEntry.insert(0, "ping -n 1 -w 3000")
+
+    def get_command(self) -> list[str]:
+        new_command: str = self.previewEntry.get()
+        return new_command.split(" ")
 
 
 class App(ctk.CTk):
